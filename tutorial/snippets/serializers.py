6b8e5e144cf2b298388c9d3f9d2a202faf46e13d
gr421nidan <nikushkina.dasha@yandex.ru>
2023-11-08 11:18:16 +0700

    Добавление разрешений

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import User
 from .models import Snippet
 
 
+
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
